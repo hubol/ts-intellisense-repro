680cf3ecc7fab0ab0259a6391bd960f54f0cb65c
Hubol <hubol.gordon@gmail.com>
2023-10-20 19:26:10 -0500

    Add comments

--- index.ts
+++ index.ts
@@ -6,5 +6,11 @@ interface Object {
     myObjectAugmentation: number;
 }
 
+// Using the <Trigger Suggest> command (Ctrl-Space), I see .myArrayAugmentation suggested
+// But not .myObjectAugmentation
 const array = [].myArrayAugmentation
+
+// Using the <Trigger Suggest> command, I do not see anything suggested
 const object = {}.myObjectAugmentation
+
+// However, the file does compile!
